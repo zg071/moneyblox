f1fdedf4fe749f64596a34676025fa07ed87aa81
zg071 <pelemeuunero@gmail.com>
2026-09-12 19:18:06 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 local placeId = game.PlaceId
+local creatorId = game.CreatorId
 
-if placeId == 7336302630 then
+if placeId == 7336302630 or (game.CreatorType == Enum.CreatorType.Group and creatorId == 3765739) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/zg071/moneyblox/refs/heads/main/project_delta.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/zg071/moneyblox/refs/heads/main/universal.lua"))()
