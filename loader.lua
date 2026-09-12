361c91123521e83db77c2fe7f545e18541e1bb6f
zg071 <pelemeuunero@gmail.com>
2026-09-12 17:12:13 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local placeId = game.PlaceId
+
+if placeId == 7336302630 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zg071/moneyblox/refs/heads/main/project_delta.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zg071/moneyblox/refs/heads/main/universal.lua"))()
+end
