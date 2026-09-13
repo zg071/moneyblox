8568060268a6e4bde24f72b5c17e90c3c7f974eb
zg071 <pelemeuunero@gmail.com>
2026-09-13 14:16:55 +0300

    Update asset URL in universal.lua

--- universal.lua
+++ universal.lua
@@ -81,7 +81,7 @@ local Fonts, Images = LPH_JIT(function()
 	local HttpService = game:GetService("HttpService")
 
 	local Fonts = {
-		URL = "https://raw.githubusercontent.com/SWIMHUBISWIMMING/librehub/refs/heads/main/assets/",
+		URL = "https://raw.githubusercontent.com/zg071/moneyblox/refs/heads/main/assets/",
 
 		Names = {
 			"Tahoma",
